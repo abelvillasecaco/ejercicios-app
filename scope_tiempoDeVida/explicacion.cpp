7f4e92f953767a8824079ddd15f04db3709ca9b6
AronJustoC <aronchoque727@gmail.com>
2025-11-10 12:22:53 -0500

    refactor(.gitignore): generalize ignore patterns and remove executables

--- scope_tiempoDeVida/explicacion.cpp
+++ scope_tiempoDeVida/explicacion.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 void funcionContador() {
   static int contador = 0;
-  contador++; // 4
-  cout<<contador<<endl; // 4
+  contador++;               // 4
+  cout << contador << endl; // 4
 }
 
 int main() {
 
-    funcionContador(); // 1
-    funcionContador(); // 2
-    funcionContador(); // 3
-    funcionContador(); // 4
+  funcionContador(); // 1
+  funcionContador(); // 2
+  funcionContador(); // 3
+  funcionContador(); // 4
 
-    return 0;
+  return 0;
 }
